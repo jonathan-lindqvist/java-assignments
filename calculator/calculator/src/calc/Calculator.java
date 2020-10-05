6745e53d2278d62424797d13084cd2624093fb32
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2020-10-05 08:26:10 +0200

    Final version of calc

--- calculator/calculator/src/calc/Calculator.java
+++ calculator/calculator/src/calc/Calculator.java
@@ -40,7 +40,21 @@ class Calculator {
     // ------  Evaluate RPN expression -------------------
 
     public double evalPostfix(List<String> postfix) {
-        return 0;  // TODO
+        Stack<String> stack = new Stack<>();
+
+        for (String token : postfix) {
+            if (OPERATORS.contains(token)) {
+                double d1 = Double.parseDouble(stack.pop());
+                double d2 = Double.parseDouble(stack.pop());
+                double res = applyOperator(token, d1, d2);
+                stack.push(String.valueOf(res));
+            } else { // Is number.
+                stack.push(token);
+            }
+        }
+
+        // Convert last remaining token on stack to double.
+        return Double.parseDouble(stack.pop());
     }
 
 
@@ -66,7 +80,76 @@ class Calculator {
     // ------- Infix 2 Postfix ------------------------
 
     public List<String> infix2Postfix(List<String> tokens) {
-        return null; // TODO
+        Stack<String> stack = new Stack<>();
+        List<String> postfix = new ArrayList<>();
+
+        for (String token : tokens) {
+            if (OPERATORS.contains(token) || "()".contains(token)) {
+                addToStack(token, stack, postfix);
+            } else { // Is number.
+                postfix.add(token);
+            }
+        }
+        // Transfer remaining tokens on stack to postfix.
+        while (!stack.isEmpty()) {
+            postfix.add(stack.pop());
+        }
+
+        return postfix;
+    }
+
+    private void addToStack(String token, Stack<String> stack, List<String> postfix) {
+        if (shouldBeginPopStack(token, stack)) {
+
+            popStack(token, stack, postfix);
+
+        } else if (token.equals(")")) {
+
+            closeParenthesis(stack, postfix);
+            // Don't push right parenthesis to stack.
+            return;
+        }
+
+        // Add new token to stack.
+        stack.push(token);
+    }
+
+    private boolean shouldBeginPopStack(String token, Stack<String> stack) {
+        boolean stackNotEmpty = !stack.isEmpty();
+        boolean tokenIsOperator = OPERATORS.contains(token);
+        boolean stackIsOperator = stackNotEmpty && OPERATORS.contains(stack.peek());
+
+        return stackNotEmpty && tokenIsOperator && stackIsOperator;
+    }
+
+    private void popStack(String token, Stack<String> stack, List<String> postfix) {
+        // While there is something to pop.
+        while (shouldPop(stack, token)) {
+            // Remove from stack and add to postfix.
+            String stackOp = stack.pop();
+            postfix.add(stackOp);
+        }
+    }
+
+    private boolean shouldPop(Stack<String> stack, String token) {
+        boolean notEmpty = !stack.isEmpty();
+        boolean leftAssociative = getAssociativity(token) == Assoc.LEFT;
+        boolean higherOrEqualPrecedence = notEmpty && getPrecedence(stack.peek()) >= getPrecedence(token);
+
+        return notEmpty && leftAssociative && higherOrEqualPrecedence;
+    }
+
+    private void closeParenthesis(Stack<String> stack, List<String> postfix) {
+        // Loop until opening parenthesis or end of stack.
+        while (!stack.isEmpty() && !stack.peek().equals("(")) {
+            String popped = stack.pop();
+            // Don't add parenthesis to postfix.
+            if (!"()".contains(popped)) {
+                postfix.add(popped);
+            }
+        }
+        // Remove left parenthesis.
+        stack.pop();
     }
 
 
@@ -100,8 +183,45 @@ class Calculator {
     // ---------- Tokenize -----------------------
 
     public List<String> tokenize(String expr) {
-        return null;   // TODO
+        // Filtrate anything that is not a digit or an operator or a parenthesis or a comma or a period.
+        expr = expr.replaceAll("[^-+*/^() ,.[0-9]]*", "");
+
+        // Split at every operand and parenthesis.
+        List<String> tokens = new ArrayList<>(Arrays.asList(expr.split("(?<=[-+*/()^ ])|(?=[-+*/()^ ])")));
+
+        // Remove tokens containing only a space.
+        tokens.removeIf(" "::equals);
+        for(int i = 0; i < tokens.size(); i++) {
+            // Replace all periods and commas with a single period.
+            tokens.set(i, tokens.get(i).replaceAll("(\\.+)|(,+)", "."));
+        }
+
+        // Check for errors in input.
+        validateInput(tokens);
+
+        return tokens;
     }
 
-    // TODO Possibly more methods
+    private void validateInput(List<String> filteredTokens) {
+        // Count stuff.
+        int operatorCount = 0, operandCount = 0, openParenCount = 0, closeParenCount = 0;
+        for (String token : filteredTokens) {
+            if (OPERATORS.contains(token)) {
+                operatorCount++;
+            } else if (token.equals("(")) {
+                openParenCount++;
+            } else if (token.equals(")")) {
+                closeParenCount++;
+            } else {
+                operandCount++;
+            }
+        }
+        if (operatorCount >= operandCount) {
+            throw new IllegalArgumentException(MISSING_OPERAND);
+        }
+        if (operandCount > operatorCount + 1 ||
+                openParenCount != closeParenCount) {
+            throw new IllegalArgumentException(MISSING_OPERATOR);
+        }
+    }
 }
